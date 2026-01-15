f3ca37623db3d81417719d97629ca1a884d1ebc7
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-01-15 10:23:07 +0530

    initial commit

---
+++ conditional_statements_P&Q/2nd.py
@@ -0,0 +1,5 @@
+char = input("Enter a character which you want to check whether it's vowel or consonant: ")
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print(f"The character {char} is a vowel.")
+else:
+    print(f"The character {char} is a consonant.")
