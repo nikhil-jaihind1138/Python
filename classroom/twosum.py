74bad452f57d65f53a6a77ab6f1a60e8c22bc725
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-01-18 19:26:10 +0530

    Updated and added something new

--- classroom/twosum.py
+++ classroom/twosum.py
@@ -1,6 +1,19 @@
-l = [18, 19, 8, 7, 6, 7]
-n = len(l)
-for i in range(n):
-    for j in range(i+1, n):
-        if l[i]+l[j] == 15:
-            print(f"The sum of the two numbers {l[i]} and {l[j]} is 15")
+"""Write a python program to check whether
+the sum of two numbers of a list is equals to the a target value or not?"""
+
+
+def twosum(nums, target):
+    dic = {}
+    for i in range(len(nums)):
+        value = target - nums[i]
+        if value in dic:
+            return [dic[value], i]
+        dic[nums[i]] = i
+    return []
+
+
+nums = [2, 7, 13, 21]
+target = 9
+call = twosum(nums, target)
+m, n = call
+print(f"The Numbers are {nums[m]} and {nums[n]}")
