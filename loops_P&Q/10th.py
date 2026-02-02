8eb4334cba2a46718d65a87f0aa7f56f6a4c059d
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-02-02 12:17:55 +0530

    2nd Feb Classroom Problems

--- loops_P&Q/10th.py
+++
@@ -1,6 +0,0 @@
-""" Calculate the product of all numbers from 1 to 10 """
-
-product = 1
-for i in range(1, 11):
-    product *= i
-print(f"total value of product is {product}")
--- loops_P&Q/9th.py
+++
@@ -1,6 +0,0 @@
-""" Calculate the sum of all numbers from 1 to 10 """
-
-sum = 0
-for i in range(1, 11):
-    sum+=i
-print(f"total value of sum is {sum}")
