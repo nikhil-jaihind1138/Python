90060827eafe96aa997f705b5d0ba65f26aedc98
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-02-17 09:28:33 +0530

    Leetcode problem number 237

---
+++ classroom/leetcode237.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        # Method 1
+        
+        while node and node.next:
+            node.val = node.next.val
+            node = node.next
+        node.next = None
+        
+        # Method 2
+        
+        node.val = node.next.val
+        node.next = node.next.next
